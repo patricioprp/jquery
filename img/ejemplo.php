78a9056b40881c46ac88d08db87664d551eb1cce
Patricio Polito <patricioprp06@gmail.com>
2018-05-14 00:34:28 -0300

    video 28

---
+++ img/ejemplo.php
@@ -0,0 +1,4 @@
+<?php
+$nombre= $_GET["nombre"];
+echo "hola".":".$nombre;
+?>
---
+++ img/ejemplo2.php
@@ -0,0 +1,5 @@
+<?php
+$nombre= $_POST["nombre"];
+$metodo= $_POST["post"];
+echo "Hola ".$nombre." uso el metodo ".$metodo;
+?>
